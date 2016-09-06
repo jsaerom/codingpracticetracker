e02cf2afa3236634043db7beedc8abf767d5b518
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-09-06 11:30:42 -0700

    Updates javascript with ability to capture/store user information add it to total hours and spit out statement based on total hours

--- CPT.java
+++ CPT.java
@@ -3,18 +3,28 @@ import java.io.Console;
 public class CPT {
   public static void main(String[] args) {
 
+    Integer totalHours = 0;
+    Integer sundayHours = askHowManyHoursStudy("Sunday");
+    totalHours += sundayHours;
+    Integer mondayHours = askHowManyHoursStudy("Monday");
+    totalHours += mondayHours;
+    Integer tuesdayHours = askHowManyHoursStudy("Tuesday");
+    totalHours += tuesdayHours;
 
-    askHowManyHoursStudy("Sunday");
 
-    askHowManyHoursStudy("Monday");
-
-    askHowManyHoursStudy("Tuesday");
+    if (totalHours > 5) {
+      System.out.println("Wow you're a rockstar");
+    } else {
+      System.out.println("You should work harder");
+    }
   }
 
-  public static void askHowManyHoursStudy(String day) {
+  public static int askHowManyHoursStudy(String day) {
     Console myConsole = System.console();
     System.out.println("How many hours did you spend studying on " + day + "?");
-    Integer hoursSpent = Integer.parseInt(myConsole.readLine());
-    System.out.println("You spent " + hoursSpent + " hours on " + day + ".");
+    String stringHoursSpent = myConsole.readLine();
+    Integer hoursSpent = Integer.parseInt(stringHoursSpent);
+    return hoursSpent;
   }
+
 }
